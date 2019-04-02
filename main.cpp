74a25ba97a3852634835e851a41b870c0da2d299
Misha(BekrenevMA) <Misha@yandex.ru>
2019-04-02 14:50:10 +0300

    code:Исправлена функция рассчета кол-ва чисел в столбцах гистограммы

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ vector<double> input_numbers(size_t count) {
     return result;
 }
 
-void find_minmax(vector<double> numbers, double& min, double& max) {
+void find_minmax(const vector<double>& numbers, double& min, double& max) {
     min = numbers[0];
     max = numbers[0];
     for (double number : numbers)
@@ -27,8 +27,11 @@ void find_minmax(vector<double> numbers, double& min, double& max) {
     }
 }
 
-vector<size_t> make_histogram(vector<double> numbers, size_t bin_count, double min, double max)
+vector<size_t> make_histogram(const vector<double>& numbers, size_t bin_count)
 {
+    double min, max;
+    find_minmax(numbers, min, max);
+
     vector<size_t> result(bin_count);
     for (double number : numbers)
     {
@@ -50,17 +53,14 @@ int main()
     cin >> number_count;
 
     cerr << "Enter numbers: ";
-    const vector<double>&numbers = input_numbers(number_count);
+    const auto numbers = input_numbers(number_count);
 
     size_t bin_count;
     cerr << "Enter column count: ";
     cin >> bin_count;
 
     // Обработка данных
-    double min, max;
-    find_minmax( numbers, min, max);
-
-    const auto bins = make_histogram(numbers, bin_count, min, max);
+    const auto bins = make_histogram(numbers, bin_count);
 
     // Вывод данных
     const size_t SCREEN_WIDTH = 80;
